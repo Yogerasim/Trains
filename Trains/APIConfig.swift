1f1316e928c6cead207fa8729248f1f317958019
PhilipGerasim <yogerrasim@gmail.com>
2025-11-21 21:12:52 +0300

    add LaunchScreen.storyboard

--- Trains/APIConfig.swift
+++ Trains/APIConfig.swift
@@ -4,7 +4,7 @@ import OpenAPIURLSession
 
 enum APIConfig {
     static let apiKey = "358e8b9d-a92c-4b0b-a840-9ca909f976d8"
-    static let runTests = true
+    static let runTests = false
     static let client: Client = {
         return Client(
             serverURL: try! Servers.Server1.url(),
